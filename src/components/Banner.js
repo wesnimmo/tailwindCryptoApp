27e5e108123f75f55c8946fe56cfa699f16c5e4c
Wes <wesnimmo@gmail.com>
2024-01-17 21:18:45 -0800

    banner adjustment

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,7 +4,7 @@ import Trending from './Trending'
 const Banner = () => {
     return (
         <div className="h-[175px] flex flex-col items-center justify-center text-white bg-[url('../banner2.jpg')]">
-            <h3 className="text-[23px] mb-5 font-bold">Trending Coins</h3>
+            <h3 className="text-[23px] mb-5 font-bold">Trending Coins Yo</h3>
             {/* <p>Get all the info regarding your favorite crypto currency</p> */}
             <Trending/>
         </div>
